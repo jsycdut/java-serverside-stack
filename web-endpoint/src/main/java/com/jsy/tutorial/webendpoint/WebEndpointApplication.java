50267e42c7f477a35a0703db3af6a56ad731082c
jsycdut <jsycdut@gmail.com>
2024-03-31 22:12:54 +0800

    feat: init

---
+++ web-endpoint/src/main/java/com/jsy/tutorial/webendpoint/WebEndpointApplication.java
@@ -0,0 +1,13 @@
+package com.jsy.tutorial.webendpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebEndpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebEndpointApplication.class, args);
+	}
+
+}
---
+++ web-endpoint/src/test/java/com/jsy/tutorial/webendpoint/WebEndpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsy.tutorial.webendpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebEndpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
